4d259da935ebd956d8385a217ee83e0a3674245d
Zidane Innis <zidaneinnis@gmail.com>
2022-05-11 17:23:16 -0500

    Looping Arrays: forEach

--- script.js
+++ script.js
@@ -106,7 +106,7 @@ console.log([...arr, ...arr2]);
 
 // JOIN
 console.log(letters.join(' - ')); //a - b - c - d -  e - f - g - h - i - j
-*/
+
 //////////////////////////////////////////
 // The new at Method
 
@@ -121,3 +121,29 @@ console.log(arr.at(-1));
 
 console.log('jonas'.at(0));
 console.log('jonas'.at(-1));
+*/
+//////////////////////////////////////////
+// Looping Arrays: forEach
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+for (const [i, movement] of movements.entries()) {
+  if (movement > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
+  }
+}
+
+console.log('----------forEach----------------');
+movements.forEach(function (mov, i, arr) {
+  if (mov > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${mov}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`);
+  }
+});
+// 0: function(200)
+// 1: function(450)
+// 2: function(400)
+// ....
