529be7671fb716dd7f4d53884e7ef461947332d8
Zidane Innis <zidaneinnis@gmail.com>
2022-05-19 20:22:43 -0500

    flat and flatMap Lecture

--- script.js
+++ script.js
@@ -577,9 +577,9 @@ for (const acc of accounts) {
     console.log('Jessica Davis');
   }
 }
-*/
 
-// some and every lecture completed
+
+// some and every Lecture
 
 console.log(movements);
 
@@ -602,3 +602,27 @@ const deposit = mov => mov > 0;
 console.log(movements.some(deposit));
 console.log(movements.every(deposit));
 console.log(movements.filter(deposit));
+*/
+
+// flat and flatMap Lecture
+
+const arr = [[1, 2, 3], [4, 5, 6], 7, 8];
+console.log(arr.flat());
+
+const arrDeep = [[[1, 2], 3], [4, [5, 6]], 7, 8];
+console.log(arrDeep.flat(2));
+
+// flat
+
+const overallBalance = accounts
+  .map(acc => acc.movements)
+  .flat()
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(overallBalance);
+
+// flatMap
+
+const overallBalance2 = accounts
+  .flatMap(acc => acc.movements)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(overallBalance);
